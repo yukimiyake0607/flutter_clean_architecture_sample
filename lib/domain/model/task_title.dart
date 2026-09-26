5fea8478b91781e3a69fb4a79d4ad014303770b0
三宅柚希 <yukimiyake38@gmail.com>
2026-09-26 21:21:47 +0900

    Add documentation for TaskTitle class, detailing encapsulation of business rules related to task titles.

--- lib/domain/model/task_title.dart
+++ lib/domain/model/task_title.dart
@@ -3,6 +3,9 @@ import 'package:flutter_clean_architecture_sample/shared/result.dart';
 /// TaskTitleが空だった場合にResult.errorに載せる例外。
 class EmptyTaskTitleException implements Exception {}
 
+/// TaskTitle（値オブジェクト）
+/// 
+/// Taskのタイトルに関するビジネスルールをここでカプセル化する。
 class TaskTitle {
   const TaskTitle._(this.value);
 
